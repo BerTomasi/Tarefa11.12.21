57ab479e6b5f896f5c386b86b7df198cdb7bccd3
berTomasi <bernardotomasi.pf002@academico.ifsul.edu.br>
2021-12-14 10:34:19 -0300

    Commit da nova branch

---
+++ src/NFClasse1.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+/*
+ * @author Bernardo Dirceu Tomasi
+ */
+public class NFClasse1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Nova Funçao - Classe 1");
+        
+
+    }
+    
+}
---
+++ src/NFClasse4.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+/*
+ * @author Bernardo Dirceu Tomasi
+ */
+public class NFClasse4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Nova Funçao - Classe 4");
+        
+    }
+    
+}
