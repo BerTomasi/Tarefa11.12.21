601df0c3079b635bc6d47f51beb50316dbad0a53
berTomasi <bernardotomasi.pf002@academico.ifsul.edu.br>
2021-12-14 10:13:15 -0300

    Commit das classes

---
+++ src/Classe2.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+/*
+ * @author Bernardo Dirceu Tomasi
+ */
+public class Classe2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Classe 2");
+    }
+    
+}
---
+++ src/Classe3.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+/*
+ * @author Bernardo Dirceu Tomasi
+ */
+public class Classe3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Classe 3");
+    }
+    
+}
